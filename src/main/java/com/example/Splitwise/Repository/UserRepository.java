0b1b3b5788d201549d05064794a1a70f670cfa87
ramanaguru <ramanaguruloyola@gmail.com>
2024-02-03 18:07:57 +0530

    Updated UserRepository

--- src/main/java/com/example/Splitwise/Repository/UserRepository.java
+++ src/main/java/com/example/Splitwise/Repository/UserRepository.java
@@ -8,5 +8,6 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
-    Optional<User> findByEmail(String email);
+    Optional<User> findByEmail(String email); // find user by email
+    Optional<User> findById(Long Id);         // find user by  id
 }
