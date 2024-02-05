66dc2cafc0c44a14ac3e4845e03424651e79d617
ramanaguru <ramanaguruloyola@gmail.com>
2024-02-05 07:29:46 +0530

    ADDED AddExpenseRequestDTO

---
+++ src/main/java/com/example/Splitwise/DTOs/AddExpenseRequestDTO.java
@@ -0,0 +1,33 @@
+package com.example.Splitwise.DTOs;
+
+import com.example.Splitwise.commands.PayType;
+import com.example.Splitwise.commands.SplitType;
+import lombok.Getter;
+import lombok.Setter;
+
+
+import java.util.List;
+
+@Getter
+@Setter
+public class AddExpenseRequestDTO {
+    private Long createdByUserId;
+
+    private List<Long> users;
+
+    private List<Long>PaidAmounts;
+
+    private List<Long> SplitAmounts;
+
+    private Long groupId;
+
+    private Long totalAmount;
+
+    private String description;
+
+    private SplitType splitType;
+
+    private PayType payType;
+
+
+}
