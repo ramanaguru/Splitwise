dd849ec24a466efb887943f1ce4923c7c2e58d88
ramanaguru <ramanaguruloyola@gmail.com>
2024-02-05 18:31:37 +0530

    ADDED ListGroupsResponseDTO

---
+++ src/main/java/com/example/Splitwise/DTOs/ListGroupsResponseDTO.java
@@ -0,0 +1,13 @@
+package com.example.Splitwise.DTOs;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ListGroupsResponseDTO {
+
+    private String message;
+    private ResponseStatus status;
+
+}
