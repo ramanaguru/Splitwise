1bb30b69077ee17bcf21fcdcb6013ccdd1fe02fa
ramanaguru <ramanaguruloyola@gmail.com>
2024-02-05 08:32:52 +0530

    ADDED AddMemberResponseDTO

---
+++ src/main/java/com/example/Splitwise/DTOs/AddMemberResponseDTO.java
@@ -0,0 +1,12 @@
+package com.example.Splitwise.DTOs;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AddMemberResponseDTO {
+
+    private String message;
+    private ResponseStatus status;
+}
