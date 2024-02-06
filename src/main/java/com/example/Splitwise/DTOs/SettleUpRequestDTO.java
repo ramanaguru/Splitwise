1327d2b92b2a62dec04cda6760a71417db364aa5
ramanaguru <ramanaguruloyola@gmail.com>
2024-02-06 13:04:16 +0530

    ADDED SettleUpRequestDTO

---
+++ src/main/java/com/example/Splitwise/DTOs/SettleUpRequestDTO.java
@@ -0,0 +1,11 @@
+package com.example.Splitwise.DTOs;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SettleUpRequestDTO {
+    private Long UserId;
+    private Long GroupId;
+}
